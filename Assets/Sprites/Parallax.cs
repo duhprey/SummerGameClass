8029586bd9bd360d31c6f6cfe28a0f5b989d0604
Nathan Fabian <ndfabian@gmail.com>
2016-04-08 17:34:06 -0600

    Basic platformer stuff.

---
+++ Assets/Sprites/Parallax.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+
+	public Transform background;
+	public Transform midground;
+
+	public Vector3 backgroundParallaxScale;
+	public Vector3 midgroundParallaxScale;
+
+	private Vector3 lastPosition;
+
+	void Start () {
+		lastPosition = transform.position;
+	}
+
+	void Update () {
+		Vector3 move = transform.position - lastPosition;
+		background.position += Vector3.Scale (move, backgroundParallaxScale);
+		midground.position += Vector3.Scale (move, midgroundParallaxScale);
+		lastPosition = transform.position;
+	}
+}
