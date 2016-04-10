7585030018a1c9a01c2886f8d2bf617728ff5b15
Nathan Fabian <ndfabian@gmail.com>
2016-04-10 16:06:17 -0600

    Destroyable blocks get damaged.

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -6,6 +6,10 @@ public class Destroyable : MonoBehaviour {
 	public float maxHitpoints = 100.0f;
 	private float currentHitpoints;
 
+	public Sprite damaged1;
+	public Sprite damaged2;
+	public Sprite damaged3;
+
 	public float CurrentHitpoints {
 		get { return currentHitpoints; }
 	}
@@ -17,7 +21,13 @@ public class Destroyable : MonoBehaviour {
 	public void TakeDamage (float damage) {
 		currentHitpoints -= damage;
 
-		// TODO replace sprites with damaged versions
+		if (currentHitpoints / maxHitpoints < .25f) {
+			GetComponent<SpriteRenderer> ().sprite = damaged3;
+		} else if (currentHitpoints / maxHitpoints < 0.5f) {
+			GetComponent<SpriteRenderer> ().sprite = damaged2;
+		} else if (currentHitpoints / maxHitpoints < 0.75f) {
+			GetComponent<SpriteRenderer> ().sprite = damaged1;
+		}
 
 		if (currentHitpoints <= 0) {
 			// TODO instantiate the inventory block
