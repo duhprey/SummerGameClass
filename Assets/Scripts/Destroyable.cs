47512b5ebf2d4bcd4139eb2253635c5ea21af95b
Nathan Fabian <ndfabian@gmail.com>
2016-04-16 08:30:39 -0600

    Added the front screen HUD

--- Assets/Scripts/Destroyable.cs
+++ Assets/Scripts/Destroyable.cs
@@ -10,6 +10,8 @@ public class Destroyable : MonoBehaviour {
 	public Sprite damaged2;
 	public Sprite damaged3;
 
+	public Transform[] collectablePrefabs;
+
 	public float CurrentHitpoints {
 		get { return currentHitpoints; }
 	}
@@ -21,16 +23,23 @@ public class Destroyable : MonoBehaviour {
 	public void TakeDamage (float damage) {
 		currentHitpoints -= damage;
 
-		if (currentHitpoints / maxHitpoints < .25f) {
+		if (currentHitpoints / maxHitpoints < .25f && damaged3 != null) {
 			GetComponent<SpriteRenderer> ().sprite = damaged3;
-		} else if (currentHitpoints / maxHitpoints < 0.5f) {
+		} else if (currentHitpoints / maxHitpoints < 0.5f && damaged2 != null) {
 			GetComponent<SpriteRenderer> ().sprite = damaged2;
-		} else if (currentHitpoints / maxHitpoints < 0.75f) {
+		} else if (currentHitpoints / maxHitpoints < 0.75f && damaged1 != null) {
 			GetComponent<SpriteRenderer> ().sprite = damaged1;
 		}
 
 		if (currentHitpoints <= 0) {
-			// TODO instantiate the inventory block
+			Collider2D col = GetComponent<Collider2D>();
+			if (collectablePrefabs != null) {
+				foreach (Transform prefab in collectablePrefabs) {
+					if (prefab != null) {
+						Instantiate (prefab, transform.position + Vector3.Scale (col.bounds.size * 0.5f, Random.insideUnitCircle), Quaternion.identity);
+					}
+				}
+			}
 			Destroy (gameObject);
 		}
 	}
---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryManager : MonoBehaviour {
+
+	public Toggle[] inventorySlots;
+	public Text[] inventoryLabels;
+
+	int firstEmpty = 0;
+
+	class InventoryItem {
+		public int index;
+		public int count;
+	}
+	private Dictionary<Transform, InventoryItem> inventory;
+
+	private void Start () {
+		inventory = new Dictionary<Transform, InventoryItem> ();
+		for (int i = 0; i < inventoryLabels.Length; i ++) {
+			inventoryLabels[i].text = "";
+		}
+	}
+
+	public void OnCollisionEnter2D (Collision2D collision) {
+		Block block = collision.gameObject.GetComponent<Block> ();
+		if (block != null) {
+			Transform prefab = block.placeablePrefab;
+			if (inventory.ContainsKey (prefab) && inventory[prefab].count < 99) {
+				inventory[prefab].count ++;
+			} else {
+				InventoryItem item = new InventoryItem ();
+				item.count = 1;
+				item.index = firstEmpty;
+				firstEmpty ++;
+				inventory.Add (prefab, item);
+			}
+			UpdateGUI (prefab);
+			Destroy (block.gameObject);
+		}
+	}
+
+	private void UpdateGUI (Transform prefab) {
+		int index = inventory[prefab].index;
+		inventorySlots[index].graphic.GetComponent<Image>().sprite = prefab.GetComponent<SpriteRenderer>().sprite;
+		inventoryLabels[index].text = "" + inventory[prefab].count;
+	}
+}
