7e307c1baa78f68cd276619eb6547d75e03a1195
Nathan Fabian <ndfabian@gmail.com>
2016-04-22 20:05:30 -0600

    Can place objects, mostly.

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -6,9 +6,11 @@ using System.Collections.Generic;
 public class InventoryManager : MonoBehaviour {
 
 	public Toggle[] inventorySlots;
+	public Image[] inventoryIcons;
 	public Text[] inventoryLabels;
 
-	int firstEmpty = 0;
+	private int firstEmpty = 0;
+	private int activeSlot;
 
 	class InventoryItem {
 		public int index;
@@ -18,6 +20,10 @@ public class InventoryManager : MonoBehaviour {
 
 	private void Start () {
 		inventory = new Dictionary<Transform, InventoryItem> ();
+		for (int i = 0; i < inventoryIcons.Length; i ++) {
+			inventoryIcons[i].color = Color.clear;
+		}
+
 		for (int i = 0; i < inventoryLabels.Length; i ++) {
 			inventoryLabels[i].text = "";
 		}
@@ -43,7 +49,29 @@ public class InventoryManager : MonoBehaviour {
 
 	private void UpdateGUI (Transform prefab) {
 		int index = inventory[prefab].index;
-		inventorySlots[index].graphic.GetComponent<Image>().sprite = prefab.GetComponent<SpriteRenderer>().sprite;
+		inventoryIcons[index].sprite = prefab.GetComponent<SpriteRenderer>().sprite;
+		inventoryIcons[index].color = Color.white;
 		inventoryLabels[index].text = "" + inventory[prefab].count;
+	}
+
+	private Transform FindItem (int index) {
+		foreach (Transform item in inventory.Keys) {
+			if (inventory[item] != null && inventory[item].index == index) {
+				return item;
+			}
+		}
+		return null;
+	}
+
+	public void Slot1Active (bool on) { if (on) SetActiveSlot (0); }
+	public void Slot2Active (bool on) { if (on) SetActiveSlot (1); }
+	public void Slot3Active (bool on) { if (on) SetActiveSlot (2); }
+	public void Slot4Active (bool on) { if (on) SetActiveSlot (3); }
+	public void Slot5Active (bool on) { if (on) SetActiveSlot (4); }
+
+	private void SetActiveSlot (int index) {
+		activeSlot = index;
+		Transform item = FindItem (index);
+		GetComponent<UseEquipment>().SetCurrentEquipment (item);
 	}
 }
--- Assets/Scripts/UseEquipment.cs
+++ Assets/Scripts/UseEquipment.cs
@@ -13,6 +13,10 @@ public class UseEquipment : MonoBehaviour {
 
 	public float damage = 10.0f;
 
+	public Vector2 gridOffset;
+	public Vector2 gridSize;
+	private Transform currentItem;
+
 	private void Update () {
 		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 
@@ -31,6 +35,9 @@ public class UseEquipment : MonoBehaviour {
 				HitTarget (hit);
 				nextHit = Time.time + timeBetweenHits;
 			}
+			if (Input.GetMouseButtonDown (1) && hit.collider.gameObject.layer == LayerMask.NameToLayer ("Ground")) {
+				PlaceObject (hit);
+			}
 			aimObject.position = hit.point;
 		} else {
 			aimObject.position = transform.position + direction * mag;
@@ -44,5 +51,22 @@ public class UseEquipment : MonoBehaviour {
 			Instantiate (sparksPrefab, hit.point, Quaternion.identity);
 			destroyable.TakeDamage (damage);
 		}
+	}
+
+	private void PlaceObject (RaycastHit2D hit) {
+		Vector3 position = hit.collider.transform.position;
+		float dx = hit.point.x - position.x;
+		float dy = hit.point.y - position.y;
+		if (Mathf.Abs (dx) > Mathf.Abs (dy)) {
+			position += Mathf.Sign (dx) * gridSize.x * Vector3.right;
+		} else {
+			position += Mathf.Sign (dy) * gridSize.y * Vector3.up;
+		}
+		Instantiate (currentItem, position, Quaternion.identity);
+		// TODO remove one from the inventory
+	}
+
+	public void SetCurrentEquipment (Transform item) {
+		currentItem = item;
 	}
 }
